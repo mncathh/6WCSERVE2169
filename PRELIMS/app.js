c65de1db1548b9af3a596acf42dc92ab264a6715
Catherine <146521776+mncathh@users.noreply.github.com>
2025-06-27 13:13:54 +0800

    Add files via upload

---
+++ PRELIMS/app.js
@@ -0,0 +1,8 @@
+var myLogModule = require('./PRELIMS/utility/log.js');
+
+myLogModule.warning('Warning node not found..');
+myLogModule.error('Error: Node encountered an error..');
+myLogModule.info('Node running..');
+
+// var msg = require('./PRELIMS/utility/Message.js');
+// console.log(msg);
---
+++ PRELIMS/utility/Message.js
@@ -0,0 +1,8 @@
+// myLogModule.warning('Warning node not found..');
+// myLogModule.error('Error: Node encountered an error..');
+// myLogModule.info('Node running..');
+
+module.exports = 'Hello World!';
+
+// var msg = require('./PRELIMS/utility/Message.js');
+// console.log(msg);
