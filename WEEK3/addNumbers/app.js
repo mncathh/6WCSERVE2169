4be41431e660a7de81f54d7af1d9bde9e64017e7
Catherine <146521776+mncathh@users.noreply.github.com>
2025-07-04 13:09:21 +0800

    Add files via upload

---
+++ WEEK3/addNumbers/app.js
@@ -0,0 +1,4 @@
+const add = require('./add');
+
+const result = add(11, 3); 
+console.log("The result is:", result);
---
+++ WEEK3/myGreetings/callGreet.js
@@ -0,0 +1,5 @@
+const greet = require('./greet');
+
+console.log(greet("John"));
+console.log(greet("Jane"));
+
